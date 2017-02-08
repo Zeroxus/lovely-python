661d0a90b0aca350ac2f466db5e63267e29f1a84
zeroxus <wjgu123@gmail.com>
2017-02-08 21:55:25 +0800

    一个汇文图书管理系统的爬虫

---
+++ opac_spider/config.py
@@ -0,0 +1,6 @@
+cookie = {'PHPSESSID':'46taljg47niamnv53o97sp1sm5'}
+targetUrl = 'http://opac.jit.edu.cn/reader/book_hist.php?page='
+header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+        (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        'Referer':'http://opac.jit.edu.cn/opac/search.php',
+        'host':'opac.jit.edu.cn', }
---
+++ opac_spider/spider.py
@@ -0,0 +1,64 @@
+import requests
+from openpyxl import Workbook,load_workbook
+from bs4 import BeautifulSoup
+from config import cookie,header,targetUrl
+
+
+class spider:
+    def __init__(self,url,cookie,session):
+        self.url = url
+        self.cookie = cookie
+        self.session = session
+
+    def makeRequest(self,pageNum):
+        s = self.session
+        url = self.getPage(pageNum)
+        req = s.get(url=url, cookies=cookie, headers=header, allow_redirects=False)
+        return req
+
+    def makeData(self,pageNum):
+        targetpage = self.makeRequest(pageNum)
+        soup = BeautifulSoup(targetpage.content,'lxml')
+        data_list=[]
+        for idx,tr in enumerate(soup.find_all('tr')):
+            if idx!=0:
+                tds = tr.find_all('td')
+                data_list.append([
+                    tds[0].contents[0],
+                    tds[1].contents[0],
+                    ''.join(tds[2].contents[0].contents),
+                    tds[3].contents[0],
+                    tds[4].contents[0],
+                    tds[5].contents[0],
+                    tds[6].contents[0],
+                ])
+        return data_list
+
+    def getPage(self,pageNum):
+        spider_url = self.url+str(pageNum)
+        print(spider_url)
+        return spider_url
+
+    def createExcel(self):
+        wb = Workbook(optimized_write=True)
+        ws = wb.create_sheet(title='书籍列表')
+        row0 = ['编号','条码号','题名','责任者','借阅日期','归还日期','馆藏地']
+        ws.append(row0)
+        save_path = 'book_hist.xlsx'
+        wb.save(save_path)
+
+    def saveToExcel(self,data_list):
+        wb = load_workbook(filename='book_hist.xlsx')
+        ws = wb.get_sheet_by_name('书籍列表')
+        for i in range(len(data_list)):
+            ws.append(data_list[i])
+        save_path = 'book_hist.xlsx'
+        wb.save(save_path)
+
+if __name__=='__main__':
+    session = requests.Session()
+    s = spider(targetUrl,cookie,session)
+    s.createExcel()
+    for i in range(1,8):
+        book_list = s.makeData(i)
+        s.saveToExcel(book_list)
