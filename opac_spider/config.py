f2f46b67ecaf57f27a46d952d9d2fd7e0feeed31
Zeroxus <wjgu123@gmail.com>
2017-05-05 23:09:36 +0800

    汇文图书管理系统抓取借阅历史

--- opac_spider/config.py
+++ opac_spider/config.py
@@ -1,4 +1,4 @@
-cookie = {'PHPSESSID':'*************************'}
+cookie = {'PHPSESSID':'************************'}
 targetUrl = 'http://opac.jit.edu.cn/reader/book_hist.php?page='
 header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
         (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
